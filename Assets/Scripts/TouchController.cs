91d2f8f9a45367dd2c2c2f6de48ebaa3602e2460
Zoran <zoran2099@gmail.com>
2024-04-25 09:56:25 -0300

    #1 Add movement with mouse button

---
+++ Assets/Scripts/TouchController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour
+{
+
+
+    [SerializeField]
+    private Vector2 pastPosition;    
+    public float Acceleration;
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+    
+        if (Input.GetMouseButton(0))
+        {
+            Move(Speed(pastPosition.x, Input.mousePosition.x, Acceleration));
+        }
+        pastPosition = Input.mousePosition;
+
+    }
+
+    private void Move(float speed)
+    {
+        transform.position += Vector3.right * Time.deltaTime * speed;
+    }
+
+    private float Speed(float currentPosition, float targetPosition, float acceleration)
+    {
+        return (targetPosition - currentPosition) * acceleration;
+    }
+}
