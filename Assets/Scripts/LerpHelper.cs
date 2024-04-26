b095f17722eb5ba2a3455a48a0a34ef8567f769b
Zoran <zoran2099@gmail.com>
2024-04-26 01:49:48 -0300

    [#1] Lesson 4: Adding obstacles

--- Assets/Scripts/LerpHelper.cs
+++ Assets/Scripts/LerpHelper.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class LerpHelper : MonoBehaviour
 {
-    public Transform target;
+    
 
-    public float lerpSpeed = 0.5f;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, target.position, lerpSpeed * Time.deltaTime);
+        
         
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,11 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Lerp")]
+    public float lerpSpeed = 0.5f;
+    public Transform target;
+    private Vector3 _position;
+
+
+    [Header("Player Settings")]
     public float speed = 1f;
+    private bool _isLive = true;
+    public string enemyTag = "Enemy";
 
     // Update is called once per frame
     void Update()
     {
+        if (!_isLive) return;
+
+        _position = target.position;
+        _position.y = transform.position.y;
+        _position.z = transform.position.z;
+
+        transform.position = Vector3.Lerp(transform.position, _position, lerpSpeed * Time.deltaTime);
+
         transform.Translate(transform.forward * speed * Time.deltaTime);
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag(enemyTag)) _isLive = false;
+    }
+
 }
