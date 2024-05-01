ca418f7adeeaec9ca9594cc01c18361ebaa26bab
Zoran <zoran2099@gmail.com>
2024-04-30 21:35:14 -0300

    [#2] Lesson 3: Invincibility Power-Up

---
+++ Assets/Scripts/PowerUps/PowerUpInvencible.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpInvencible : PowerUpBase
+{
+    protected override void StartPowerUp()
+    {
+        base.StartPowerUp();
+        PlayerController.Instance.SetInvencible(true);
+    }
+
+    protected override void EndPowerUp()
+    {
+        base.EndPowerUp();
+        PlayerController.Instance.SetInvencible(false);
+    }
+}
