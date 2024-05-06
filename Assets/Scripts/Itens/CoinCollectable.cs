3fb3d0f3108c27854c241f4c1bad74d03ac16173
Zoran <zoran2099@gmail.com>
2024-05-06 20:00:42 -0300

    [#2] Lesson 5: Adding power that collects all coins

--- Assets/Scripts/Itens/CoinCollectable.cs
+++ Assets/Scripts/Itens/CoinCollectable.cs
@@ -1,9 +1,40 @@
 using Ebac.Core.Singleton;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CoinCollectable : ItemCollectableBase
 {
-    
+
+    //public Collider collider;
+    public bool collect = false;
+    public float lerp = 5f;
+    public float minDistance = 1f;
+
+    protected override void OnCollect()
+    {
+        //base.OnCollect();
+        GetComponent<Collider>().enabled = false;
+        collect = true;
+        //PlayerController.Instance.Bounce();
+    }
+
+
+
+    private void Update()
+    {
+        if (collect)
+        {
+            transform.position = Vector3.Lerp(transform.position,
+            PlayerController.Instance.transform.position, lerp * Time.deltaTime);
+            if (Vector3.Distance(transform.position, PlayerController.Instance.transform.position) < minDistance)
+            {
+                //HideItens();
+                base.OnCollect();
+            }
+        }
+    }
+
+
 }
