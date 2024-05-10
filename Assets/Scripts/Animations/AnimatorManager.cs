70ac108ac1adf80c4ef9194938bce5c4aaabad89
Zoran <zoran2099@gmail.com>
2024-05-10 09:33:30 -0300

    [#3] Lesson 3: Adding an Animator Manager

---
+++ Assets/Scripts/Animations/AnimatorManager.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorManager : MonoBehaviour
+{
+    public enum AnimationType
+    {
+        IDLE, RUN, DEAD
+    }
+
+    public Animator animator;
+    public List<AnimatorSetup> animatorSetup;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            PlayAnimation(AnimationType.IDLE);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            PlayAnimation(AnimationType.RUN);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            PlayAnimation(AnimationType.DEAD);
+
+        }
+    }
+
+    private void PlayAnimation(AnimationType type)
+    {
+        animatorSetup.ForEach(setup => { if (setup.type == type) animator.SetTrigger(setup.trigger); });
+    }
+}
+[System.Serializable]
+public class AnimatorSetup
+{
+    public AnimatorManager.AnimationType type;
+    public string trigger;
+
+}
