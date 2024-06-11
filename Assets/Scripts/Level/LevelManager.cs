69fddf0693122281c7297f77eb32901548739d5c
Zoran <zoran2099@gmail.com>
2024-06-11 15:22:19 -0300

    [#4] - Spawn Level

--- Assets/Scripts/Level/LevelManager.cs
+++ Assets/Scripts/Level/LevelManager.cs
@@ -7,23 +7,26 @@ public class LevelManager : MonoBehaviour
 {
     public Transform container;
 
-    public GameObject level;
+    public List<GameObject> level;
+
+    [SerializeField]
+    private int _indexLevel;
     
     // Start is called before the first frame update
     void Start()
     {
-        SpawnLevel(level, container);
+        SpawnLevel(level[_indexLevel], container);
     }
 
     private void SpawnLevel(GameObject level, Transform container)
     {
         var currentLevel =  Instantiate(level, container);
 
-        if (currentLevel != null)
-        {
-            currentLevel.transform.localPosition = Vector3.zero;
+        //if (currentLevel != null)
+        //{
+        //    currentLevel.transform.localPosition = Vector3.zero;
 
-        }
+        //}
     }
 
     // Update is called once per frame
